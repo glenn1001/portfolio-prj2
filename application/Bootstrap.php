fe66353fada4207398b165d365bca3166d15ae1f
Glenn Blom <glennblom@gmail.com>
2012-12-09 20:24:30 +0100

    * change structure to module structure

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,23 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+    private $_acl = null;
+
+    protected function _initAutoload() {
+        $modelLoader = new Zend_Application_Module_Autoloader(array(
+                    'namespace' => '',
+                    'basePath' => APPLICATION_PATH . '/modules/default'
+                ));
+
+        return $modelLoader;
+    }
+
+    protected function _initPlugins() {
+        // Access plugin
+        $front = Zend_Controller_Front::getInstance();
+        $front->registerPlugin(new Plugin_LayoutCheck());
+    }
 
 }
 
