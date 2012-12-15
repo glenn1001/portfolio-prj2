4114f9d578b829ef4b3ddde80d14ecadcb75e5d9
Glenn Blom <glennblom@gmail.com>
2012-12-15 21:48:41 +0100

    * update navigation frontend

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,4 +2,22 @@
   Document   : style
   Created on : 5-dec-2012, 9:04:52
   Author     : Glenn Blom <glennblom@gmail.com>
-*/
+ */
+
+$(document).ready(function() {
+    $("#nav ul li").hover(
+        function () {
+            if ($(this).find('ul').length != 0) {
+                $(this).find('a').css("background-image" , "url('/img/pc/arrow.png')");
+                $(this).find('ul').slideDown('fast');
+            }
+            
+        }, 
+        function () {
+            if ($(this).find('ul').length != 0) {
+                $(this).find('a').css("background-image" , "url('')");
+                $(this).find('ul').slideUp('fast');
+            }
+        }
+    );
+});
