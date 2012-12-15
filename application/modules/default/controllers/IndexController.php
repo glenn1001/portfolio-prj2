9f075db956a1787696d8ab2cb01863835b3e524f
Glenn Blom <glennblom@gmail.com>
2012-12-15 14:14:35 +0100

    * add headMeta * add headLink (canonical)

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -7,14 +7,12 @@ class Default_IndexController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
-        $head_info = array(
-            'title' => 'Portfolio - Glenn Blom',
-            'meta_description' => 'Portfolio website van Glenn Blom. Gebouwd in Zend Framework',
-            'meta_keywords' => 'Zend, Framework, Portfolio, Glenn, Blom',
-            'canonical' => 'http://portfolio-prj2.glennblom.nl/'
-        );
-        
-        $this->view->assign('head_info', $head_info);
+        $this->view->headMeta()->appendName('description', 'Portfolio website van Glenn Blom. Gebouwd in Zend Framework.');
+        $this->view->headMeta()->appendName('keywords', 'Zend, Framework, Portfolio, Glenn, Blom');
+        $this->view->headLink(array(
+            'rel' => 'canonical',
+            'href' => 'http://' . $this->getRequest()->getHttpHost() . '/'
+        ));
     }
 
 }
