5a23a1b4b07345360b7ed8e3be56a8742684db2c
Glenn Blom <glennblom@gmail.com>
2012-12-30 20:27:15 +0100

    * small fix for slidedown menu

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,20 +4,28 @@
   Author     : Glenn Blom <glennblom@gmail.com>
  */
 
+var slideCheck = true;
+
 $(document).ready(function() {
     $("#nav ul li").hover(
         function () {
-            if ($(this).find('ul').length != 0) {
+            if ($(this).find('ul').length != 0 && slideCheck == true) {
                 $(this).find('a').css("background-image" , "url('/img/pc/arrow.png')");
                 $(this).find('ul').slideDown('fast');
             }
             
         }, 
         function () {
-            if ($(this).find('ul').length != 0) {
+            if ($(this).find('ul').length != 0 && slideCheck == true) {
+                slideCheck = false;
                 $(this).find('a').css("background-image" , "url('')");
                 $(this).find('ul').slideUp('fast');
+                setTimeout(resetCheck, 200);
             }
         }
     );
 });
+
+function resetCheck() {
+    slideCheck = true;
+}
