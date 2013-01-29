dd91bc7f8c036abc8d96c9d02efdf88b9e4b955a
Glenn Blom <glennblom@gmail.com>
2013-01-29 16:02:48 +0100

    * add last 10 projects to footer

---
+++ application/modules/default/plugins/FooterProjects.php
@@ -0,0 +1,19 @@
+<?php
+
+class Plugin_FooterProjects extends Zend_Controller_Plugin_Abstract {
+
+    private $_view;
+
+    public function __construct() {
+        $registry = Zend_Registry::getInstance();
+        $this->_view = $registry->get('view');
+    }
+
+    public function preDispatch(Zend_Controller_Request_Abstract $request) {
+        $dbTableProject = new Default_Model_DbTable_Project();
+        $projects = $dbTableProject->fetchAll("`status`='Y'", 'pos DESC', 10);
+
+        $this->_view->footerProjects = $projects;
+    }
+
+}
